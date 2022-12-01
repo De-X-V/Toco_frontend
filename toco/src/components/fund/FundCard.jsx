57b4cc33c644a16ee2e4a6ce6018b857aa1a67a4
mangming2 <leejiho35@gmail.com>
2022-12-01 17:28:54 +0900

    Update FundCard.jsx

--- toco/src/components/fund/FundCard.jsx
+++ toco/src/components/fund/FundCard.jsx
@@ -9,9 +9,17 @@ export default function FundCard() {
     <StyledCard>
       <Image src={Logo} width="350px" height="160px" />
       <StyledDday>D-100</StyledDday>
-      <StyledProjectName>프로젝트 펀딩 이름</StyledProjectName>
-      <span>모금 종료일 </span>
-      <span> 목표 모금액</span>
+      <StyledDesWrap>
+        <StyledProjectName>프로젝트 펀딩 이름</StyledProjectName>
+        <StyledRange>
+          <div>모금 종료일 </div>
+          <div> 목표 모금액</div>
+        </StyledRange>
+        <StyledTag>
+          <div>tag1</div>
+          <div>tag2</div>
+        </StyledTag>
+      </StyledDesWrap>
     </StyledCard>
   );
 }
@@ -19,8 +27,8 @@ export default function FundCard() {
 const StyledCard = styled.div`
   width: 350px;
   height: 276px;
-  background: #ffffff;
-  border: 8px solid black;
+  background: black;
+
   border-radius: 16px;
   flex-shrink: 0;
   position: relative;
@@ -36,11 +44,35 @@ const StyledDday = styled.p`
   right: 0px;
   top: 0px;
   margin: 0;
-  background: #6d9886;
+  background: linear-gradient(
+    100.6deg,
+    #d16ba5 0%,
+    #86a8e7 50.89%,
+    #5ffbf1 97.7%
+  );
   border-radius: 0px 16px;
 `;
+const StyledDesWrap = styled.div`
+  height: 120px;
+  border: 3px solid transparent;
+  border-top: 0px;
+  background-image: linear-gradient(#545563, #545563),
+    linear-gradient(100.6deg, #d16ba5 0%, #86a8e7 50.89%, #5ffbf1 97.7%);
+  background-origin: border-box;
+  background-clip: content-box, border-box;
+  border-radius: 0px 0px 16px 16px;
+`;
+const StyledRange = styled.div`
+  display: flex;
+  color: black;
+`;
 
-const StyledProjectName = styled.p`
+const StyledTag = styled.div`
+  display: flex;
+  color: black;
+`;
+
+const StyledProjectName = styled.div`
   height: 24px;
   left: 0px;
   right: 32px;
@@ -59,7 +91,7 @@ const StyledProjectName = styled.p`
 
   /* neutral/black */
 
-  color: #2b2b43;
+  color: white;
 
   margin-top: 16px;
   margin-left: 16px;
